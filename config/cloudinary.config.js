469c6f59951155bbbe159c378f8e0934d65bb83a
Raquel Santos <raquelmrsantos@gmail.com>
2023-02-14 14:05:20 +0000

    added more cloudinary stuff

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -5,19 +5,20 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary'); // to access
 
 // your three cloudinary keys will be passed here from your .env file
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'cookbook', // The name of the folder in cloudinary . You can name this whatever you want
-    allowedFormats: ['jpg', 'png'],
+    params: {
+        folder: 'cookbook', // The name of the folder in cloudinary . You can name this whatever you want
+        allowedFormats: ['jpg', 'png'],
     // params: { resource_type: 'raw' }, => add this is in case you want to upload other type of files, not just images
     filename: function (req, res, cb) {
         cb(null, res.originalname); // The file on cloudinary will have the same name as the original file name
-    }
+    }}
 });
 
 module.exports = multer({ storage });
